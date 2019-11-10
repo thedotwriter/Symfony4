57d290fb4b701d51a84d71e1bf0d1ec6760217b6
thedotwriter <peeter.rannou@gmai.com>
2019-11-10 11:07:30 +0100

    Commit

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,11 +5,26 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController
 {
+    /**
+     * @Route("/")
+     * @return Response
+     */
     public function homepage()
     {
-        return new Response('Test');
+        return new Response('Test s');
+    }
+
+    /**
+     * @Route("/article/{slug}")
+     * @param $slug
+     * @return Response
+     */
+    public function show($slug)
+    {
+        return new Response('Article slug:' . $slug);
     }
 }
