e3ebb7f5f9466d4d289cabf7b335af30d92383bf
thedotwriter <peeter.rannou@gmai.com>
2019-11-10 10:57:47 +0100

    Commit

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,10 +4,12 @@
 namespace App\Controller;
 
 
+use Symfony\Component\HttpFoundation\Response;
+
 class ArticleController
 {
     public function homepage()
     {
-
+        return new Response('Test');
     }
 }
